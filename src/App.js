bc46b8b95fd1dfcfabd9001e5b021ed03b93b995
Bobish <bobishdayal@gmail.com>
2023-03-29 02:43:05 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Image } from "antd";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import SideMenu from "./components/SideMenu";
+import AppRoutes from "./components/AppRoutes";
+import awsconfig from "./aws-exports";
+import RestaurantContextProvider from "./contexts/RestaurantContext";
+
+const { Sider, Content, Footer } = Layout;
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RestaurantContextProvider>
+      <Layout>
+        <Sider style={{ height: "100vh", backgroundColor: "white" }}>
+          <Image
+            src="https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg"
+            preview={false}
+          />
+          <SideMenu />
+        </Sider>
+        <Layout>
+          <Content>
+            <AppRoutes />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Uber Eats Restaurant Dashboard © 2023
+          </Footer>
+        </Layout>
+      </Layout>
+    </RestaurantContextProvider>
   );
 }
-
-export default App;
+export default withAuthenticator(App);
---
+++ src/ui-components/CourierCreateForm.jsx
@@ -0,0 +1,325 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import {
+  Button,
+  Flex,
+  Grid,
+  SelectField,
+  TextField,
+} from "@aws-amplify/ui-react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Courier } from "../models";
+import { fetchByPath, validateField } from "./utils";
+import { DataStore } from "aws-amplify";
+export default function CourierCreateForm(props) {
+  const {
+    clearOnSuccess = true,
+    onSuccess,
+    onError,
+    onSubmit,
+    onValidate,
+    onChange,
+    overrides,
+    ...rest
+  } = props;
+  const initialValues = {
+    name: "",
+    sub: "",
+    lat: "",
+    lng: "",
+    transporationMode: undefined,
+  };
+  const [name, setName] = React.useState(initialValues.name);
+  const [sub, setSub] = React.useState(initialValues.sub);
+  const [lat, setLat] = React.useState(initialValues.lat);
+  const [lng, setLng] = React.useState(initialValues.lng);
+  const [transporationMode, setTransporationMode] = React.useState(
+    initialValues.transporationMode
+  );
+  const [errors, setErrors] = React.useState({});
+  const resetStateValues = () => {
+    setName(initialValues.name);
+    setSub(initialValues.sub);
+    setLat(initialValues.lat);
+    setLng(initialValues.lng);
+    setTransporationMode(initialValues.transporationMode);
+    setErrors({});
+  };
+  const validations = {
+    name: [{ type: "Required" }],
+    sub: [{ type: "Required" }],
+    lat: [],
+    lng: [],
+    transporationMode: [],
+  };
+  const runValidationTasks = async (
+    fieldName,
+    currentValue,
+    getDisplayValue
+  ) => {
+    const value = getDisplayValue
+      ? getDisplayValue(currentValue)
+      : currentValue;
+    let validationResponse = validateField(value, validations[fieldName]);
+    const customValidator = fetchByPath(onValidate, fieldName);
+    if (customValidator) {
+      validationResponse = await customValidator(value, validationResponse);
+    }
+    setErrors((errors) => ({ ...errors, [fieldName]: validationResponse }));
+    return validationResponse;
+  };
+  return (
+    <Grid
+      as="form"
+      rowGap="15px"
+      columnGap="15px"
+      padding="20px"
+      onSubmit={async (event) => {
+        event.preventDefault();
+        let modelFields = {
+          name,
+          sub,
+          lat,
+          lng,
+          transporationMode,
+        };
+        const validationResponses = await Promise.all(
+          Object.keys(validations).reduce((promises, fieldName) => {
+            if (Array.isArray(modelFields[fieldName])) {
+              promises.push(
+                ...modelFields[fieldName].map((item) =>
+                  runValidationTasks(fieldName, item)
+                )
+              );
+              return promises;
+            }
+            promises.push(
+              runValidationTasks(fieldName, modelFields[fieldName])
+            );
+            return promises;
+          }, [])
+        );
+        if (validationResponses.some((r) => r.hasError)) {
+          return;
+        }
+        if (onSubmit) {
+          modelFields = onSubmit(modelFields);
+        }
+        try {
+          Object.entries(modelFields).forEach(([key, value]) => {
+            if (typeof value === "string" && value.trim() === "") {
+              modelFields[key] = undefined;
+            }
+          });
+          await DataStore.save(new Courier(modelFields));
+          if (onSuccess) {
+            onSuccess(modelFields);
+          }
+          if (clearOnSuccess) {
+            resetStateValues();
+          }
+        } catch (err) {
+          if (onError) {
+            onError(modelFields, err.message);
+          }
+        }
+      }}
+      {...getOverrideProps(overrides, "CourierCreateForm")}
+      {...rest}
+    >
+      <TextField
+        label="Name"
+        isRequired={true}
+        isReadOnly={false}
+        value={name}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              name: value,
+              sub,
+              lat,
+              lng,
+              transporationMode,
+            };
+            const result = onChange(modelFields);
+            value = result?.name ?? value;
+          }
+          if (errors.name?.hasError) {
+            runValidationTasks("name", value);
+          }
+          setName(value);
+        }}
+        onBlur={() => runValidationTasks("name", name)}
+        errorMessage={errors.name?.errorMessage}
+        hasError={errors.name?.hasError}
+        {...getOverrideProps(overrides, "name")}
+      ></TextField>
+      <TextField
+        label="Sub"
+        isRequired={true}
+        isReadOnly={false}
+        value={sub}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              name,
+              sub: value,
+              lat,
+              lng,
+              transporationMode,
+            };
+            const result = onChange(modelFields);
+            value = result?.sub ?? value;
+          }
+          if (errors.sub?.hasError) {
+            runValidationTasks("sub", value);
+          }
+          setSub(value);
+        }}
+        onBlur={() => runValidationTasks("sub", sub)}
+        errorMessage={errors.sub?.errorMessage}
+        hasError={errors.sub?.hasError}
+        {...getOverrideProps(overrides, "sub")}
+      ></TextField>
+      <TextField
+        label="Lat"
+        isRequired={false}
+        isReadOnly={false}
+        type="number"
+        step="any"
+        value={lat}
+        onChange={(e) => {
+          let value = isNaN(parseFloat(e.target.value))
+            ? e.target.value
+            : parseFloat(e.target.value);
+          if (onChange) {
+            const modelFields = {
+              name,
+              sub,
+              lat: value,
+              lng,
+              transporationMode,
+            };
+            const result = onChange(modelFields);
+            value = result?.lat ?? value;
+          }
+          if (errors.lat?.hasError) {
+            runValidationTasks("lat", value);
+          }
+          setLat(value);
+        }}
+        onBlur={() => runValidationTasks("lat", lat)}
+        errorMessage={errors.lat?.errorMessage}
+        hasError={errors.lat?.hasError}
+        {...getOverrideProps(overrides, "lat")}
+      ></TextField>
+      <TextField
+        label="Lng"
+        isRequired={false}
+        isReadOnly={false}
+        type="number"
+        step="any"
+        value={lng}
+        onChange={(e) => {
+          let value = isNaN(parseFloat(e.target.value))
+            ? e.target.value
+            : parseFloat(e.target.value);
+          if (onChange) {
+            const modelFields = {
+              name,
+              sub,
+              lat,
+              lng: value,
+              transporationMode,
+            };
+            const result = onChange(modelFields);
+            value = result?.lng ?? value;
+          }
+          if (errors.lng?.hasError) {
+            runValidationTasks("lng", value);
+          }
+          setLng(value);
+        }}
+        onBlur={() => runValidationTasks("lng", lng)}
+        errorMessage={errors.lng?.errorMessage}
+        hasError={errors.lng?.hasError}
+        {...getOverrideProps(overrides, "lng")}
+      ></TextField>
+      <SelectField
+        label="Transporation mode"
+        placeholder="Please select an option"
+        isDisabled={false}
+        value={transporationMode}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              name,
+              sub,
+              lat,
+              lng,
+              transporationMode: value,
+            };
+            const result = onChange(modelFields);
+            value = result?.transporationMode ?? value;
+          }
+          if (errors.transporationMode?.hasError) {
+            runValidationTasks("transporationMode", value);
+          }
+          setTransporationMode(value);
+        }}
+        onBlur={() =>
+          runValidationTasks("transporationMode", transporationMode)
+        }
+        errorMessage={errors.transporationMode?.errorMessage}
+        hasError={errors.transporationMode?.hasError}
+        {...getOverrideProps(overrides, "transporationMode")}
+      >
+        <option
+          children="Driving"
+          value="DRIVING"
+          {...getOverrideProps(overrides, "transporationModeoption0")}
+        ></option>
+        <option
+          children="Bicycling"
+          value="BICYCLING"
+          {...getOverrideProps(overrides, "transporationModeoption1")}
+        ></option>
+      </SelectField>
+      <Flex
+        justifyContent="space-between"
+        {...getOverrideProps(overrides, "CTAFlex")}
+      >
+        <Button
+          children="Clear"
+          type="reset"
+          onClick={(event) => {
+            event.preventDefault();
+            resetStateValues();
+          }}
+          {...getOverrideProps(overrides, "ClearButton")}
+        ></Button>
+        <Flex
+          gap="15px"
+          {...getOverrideProps(overrides, "RightAlignCTASubFlex")}
+        >
+          <Button
+            children="Submit"
+            type="submit"
+            variation="primary"
+            isDisabled={Object.values(errors).some((e) => e?.hasError)}
+            {...getOverrideProps(overrides, "SubmitButton")}
+          ></Button>
+        </Flex>
+      </Flex>
+    </Grid>
+  );
+}
